7f6fe59c33de99eb69a4566fb5715e1b9a8f684c
Abhishek Surve <abhishek.sur@media.net>
2020-07-26 12:54:44 +0530

    W4:Q5: AddDigits

--- WEEK4/AddDigits.java
+++ WEEK4/AddDigits.java
@@ -16,4 +16,15 @@ public class AddDigits {
             return num;
         }
     }
+
+    class Solution {
+        public int addDigits(int num) {
+
+            if (num == 0) return 0;
+
+            if (num % 9 == 0) return 9;
+
+            return num % 9;
+        }
+    }
 }

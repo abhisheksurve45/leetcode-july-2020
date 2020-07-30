f6ea663370f04ca68c5e778a7acb2cfd43e05a2a
Abhishek Surve <abhishek.sur@media.net>
2020-07-30 16:19:37 +0530

    W5:Q2: WordBreakII

--- WEEK5/WordBreakII.java
+++ WEEK5/WordBreakII.java
@@ -1,3 +1,39 @@
 public class WordBreakII {
 
+    class Solution {
+
+        public List<String> wordBreak(String s, List<String> wordDict) {
+            return wordBreakHelper(s, wordDict, new HashMap<String, List<String>>());
+        }
+
+        private List<String> wordBreakHelper(String s, List<String> wordDict, Map<String, List<String>> map) {
+
+            if (map.containsKey(s)) return map.get(s);
+
+            List<String> result = new ArrayList<>();
+
+            if (s != null && s.isEmpty()) {
+                result.add("");
+                return result;
+            }
+
+            for (String word : wordDict) {
+
+                if (s.startsWith(word)) {
+
+                    List<String> subList = wordBreakHelper(s.substring(word.length()), wordDict, map);
+
+                    for (String sub : subList) {
+                        sub = sub.isEmpty() ? sub : " " + sub;
+                        result.add(word + sub);
+                    }
+
+                }
+
+            }
+
+            map.put(s, result);
+            return result;
+        }
+    }
 }
